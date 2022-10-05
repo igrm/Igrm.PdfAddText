b19dbbc65a33650c23daada71dda5939d8475964
Igor Mishur <igor.mishur@gmail.com>
2022-10-05 16:17:58 +0300

    reading page raw datastream added

--- src/main.c
+++ src/main.c
@@ -5,7 +5,11 @@
 
 int main(int argc, char** argv) 
 {
+    unsigned char* bufp = NULL;
+    size_t len = NULL;
+
     qpdf_data qpdf = qpdf_init();
+
 
     if (argc != 4)
     {
@@ -19,7 +23,19 @@ int main(int argc, char** argv)
         printf("%s opened.\n", argv[1]);
         int total_pages = qpdf_get_num_pages(qpdf);
         printf("%d pages in total\n ", total_pages);
-        
+        qpdf_oh page_object_handle = qpdf_get_page_n(qpdf, (atoi(argv[2])-1));
+        printf("Page object handle: %d\n", page_object_handle);
+        if ((qpdf_oh_get_page_content_data( qpdf, page_object_handle, &bufp, &len ) & QPDF_ERRORS) == 0)
+        {
+              printf("%d\n", len);
+              printf("%s", bufp);
+        }
+        else 
+        {
+            printf("Error while reading page %s content", argv[2]);
+            printf( "%s\n",qpdf_get_error_full_text(qpdf, qpdf_get_error(qpdf)));
+            exit(-1);
+        }
     }
     else
     {
@@ -29,5 +45,6 @@ int main(int argc, char** argv)
     }
 
     qpdf_cleanup(&qpdf);
+    free(bufp);
     return 0;
 }
